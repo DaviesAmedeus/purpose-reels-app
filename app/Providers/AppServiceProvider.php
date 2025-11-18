58259018fa8a4836f0c6fbb3e55066e34acacb1f
DaviesAmedeus <amedeusedgar@gmail.com>
2025-11-18 17:44:20 +0300

    Securing Admin Routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Support\Facades\Session;
+
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +25,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /*---- Below  are defined what 'auth' and 'guest' middleware alias should if used in the routes ----*/
+
+        // RedirectIfAuthenticated (alias 'guest'): This middle designed to prevent authenticated users from accessing certain routes that should only be accessed by guests (eg login page / registration page)
+
+        //Redirect an Authenticated user to dashboarad
+        RedirectIfAuthenticated::redirectUsing(function(){
+            return route('admin.dashboard');
+        });
+
+
+
+        // Authenticate(alias 'guest'): This middle ensures that only authenticated users can access certain routes. If user in un-authenticated will be diected back to login page.
+
+        //Redirect un-authenticated user to Login (Admin Login Page):
+        Authenticate::redirectUsing(function(){
+            Session::flash('fail', 'You must be logged in to access the admin area. Please Login to continue');
+            return route('admin.login');
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::view('/example-auth', 'example-auth');
 // ADMIN ROUTES
 Route::prefix('admin')->name('admin.')->group(function () {
 
-    Route::middleware([])->group(function () {
+    Route::middleware(['guest'])->group(function () {
         Route::controller(AuthController::class)->group(function () {
             Route::get('/login', 'loginForm')->name('login');
             Route::post('/login', 'loginHandler')->name('login_handler');
@@ -26,7 +26,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
     });
 
 
-    Route::middleware([])->group(function () {
+    Route::middleware(['auth'])->group(function () {
         Route::controller(AdminController::class)->group(function () {
             Route::get('/dashboard', 'adminDashboard')->name('dashboard');
             Route::post('/logout', 'logoutHandler')->name('logout');
