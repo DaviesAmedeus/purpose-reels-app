fe7e0806f95596762db04c2a0408ce7b2577f0fd
DaviesAmedeus <amedeusedgar@gmail.com>
2025-11-21 18:33:40 +0300

    Create user page profie:Update Admin Personal Details

--- app/Livewire/Admin/Profile.php
+++ app/Livewire/Admin/Profile.php
@@ -9,21 +9,59 @@ class Profile extends Component
 {
     public $tab = null;
     public $tabname = 'personal_details';
-    protected  $queryString = ['tab'=>['keep'=>true]];
+    protected  $queryString = ['tab' => ['keep' => true]];
 
-    public function selectTab($tab){
+    public $name, $email, $username, $bio;
+
+
+    public function selectTab($tab)
+    {
         $this->tab = $tab;
     }
 
-    public function mount(){
+    public function mount()
+    {
         $this->tab = Request('tab') ? Request('tab') : $this->tabname;
+
+        // Pupulate
+        $user = User::findOrfail(auth()->id());
+        $this->name = $user->name;
+        $this->email = $user->email;
+        $this->username = $user->username;
+        $this->bio = $user->bio;
     }
+
+    public function updatePersonalDetails()
+    {
+        $user = User::findOrfail(auth()->id());
+        $this->validate([
+            'name' => 'required',
+            'username' => 'required|unique:users,username,' . $user->id,
+        ]);
+
+        //  Update user info
+        $user->name = $this->name;
+        $user->username = $this->username;
+        $user->bio = $this->bio;
+        $updated = $user->save();
+
+        sleep(0.5);
+
+        if($updated){
+            $this->dispatch('showToastr', ['type'=>'success', 'message'=>'Your personal details have been updated successfully']);
+            $this->dispatch('updateTopUserInfo')->to(TopUserInfo::class);
+        }else{
+            $this->dispatch('showToastr', ['type'=>'error', 'message'=>'Ofcourse something went wrong!']);
+        }
+    }
+
+
 
 
     public function render()
     {
-        return view('livewire.admin.profile',[
-            'user'=>User::findOrFail(auth()->id())
+        return view('livewire.admin.profile', [
+            'user' => User::findOrFail(auth()->id())
         ]);
     }
 }
--- resources/views/livewire/admin/profile.blade.php
+++ resources/views/livewire/admin/profile.blade.php
@@ -87,10 +87,52 @@
                             </li>
                         </ul>
                         <div class="tab-content">
-                            <!-- Timeline Tab start -->
                             <div class="tab-pane fade show {{ $tab == 'personal_details' ? 'show active' : '' }}" id="personal_details" role="tabpanel">
                                 <div class="pd-20">
-                                    --- Personal Details -----
+                                    <form wire:submit="updatePersonalDetails()">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <label for="">Full name</label>
+                                                    <input type="text" class="form-control" wire:model='name' placeholder="Enter Full name">
+                                                    @error('name')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label for="">Email</label>
+                                                    <input type="text" class="form-control" wire:model='email' placeholder="Enter email address" disabled>
+                                                    @error('email')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label for="">Username</label>
+                                                    <input type="text" class="form-control" wire:model='username' placeholder="Enter username">
+                                                    @error('username')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <label for="">Bio</label>
+                                                    <textarea wire:model="bio" id="" cols="4" rows="4" class="form-control" placeholder="Type your bio"></textarea>
+                                                    @error('bio')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn-primary">Save changes</button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
 
